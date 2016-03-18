3254344953024e9219be6005730d45890f815513
Andre Ganske <andre.g.ganske@gmail.com>
2016-03-17 23:25:22 -0300

    user

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # app
 
 	creating server projects:
-		- meteor remove autopublish
-		- meteor add accounts-password
-		- meteor add simple:rest
-		- meteor add simple:json-routes
-		// - meteor add simple:simple:rest-json-error-handler
-		- meteor add simple:rest-accounts-password
+		$ meteor remove autopublish
+		$ meteor add accounts-password
+		$ meteor add fourseven:scss
+		$ meteor add reywood:bootstrap3-sass
+		$ meteor add simple:rest
--- server/server/publish.js
+++ server/server/publish.js
@@ -1,39 +1,20 @@
-if (Meteor.isServer) {
 
-	JsonRoutes.Middleware.use(JsonRoutes.Middleware.parseBearerToken);
-	JsonRoutes.Middleware.use(
-		JsonRoutes.Middleware.authenticateMeteorUserByToken
-	);
-	JsonRoutes.ErrorMiddleware.use(RestMiddleware.handleErrorAsJson);
+var Persona = new Mongo.Collection('persona');
 
-	// Setup public endpoints to lists
-	SimpleRest.configure({
-		collections: []
+Meteor.publish('personas', function() {
+	return Persona.find();
+});
+
+Meteor.publish('personas-by-name', function(name) {
+	return Persona.find({name: name});
+});
+
+/***********************************************************/
+
+Meteor.method('create', function(json) {
+	Persona.insert({
+		name: json.name,
+		createdAt: new Date(),
 	});
 
-	var Persona = new Mongo.Collection('persona');
-
-	Meteor.publish('personas', function() {
-		return Persona.find();
-	});
-
-	Meteor.publish('personas-by-name', function(name) {
-		return Persona.find({name: name});
-	},{
-		getArgsFromRequest: function(request) {
-			return [request.query.name];
-		}
-	});
-
-	/***********************************************************/
-
-	Meteor.method('create', function(json) {
-		
-		Persona.insert({
-			name: json.name,
-			createdAt: new Date(),
-		});
-
-	});
-
-}
+});
