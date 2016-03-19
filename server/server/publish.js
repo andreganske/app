603b925af184f458101935ead8f5ba8c796f3cde
Andre Ganske <andre.g.ganske@gmail.com>
2016-03-19 10:34:50 -0300

    account view

--- server/server/publish.js
+++ server/server/publish.js
@@ -1,20 +1,36 @@
 
-var Persona = new Mongo.Collection('persona');
-
-Meteor.publish('personas', function() {
-	return Persona.find();
-});
-
-Meteor.publish('personas-by-name', function(name) {
-	return Persona.find({name: name});
-});
+var Personas = new Mongo.Collection('persona');
+var Accounts = new Mongo.Collection('account');
 
 /***********************************************************/
 
-Meteor.method('create', function(json) {
-	Persona.insert({
+Meteor.publish('personas', function() {
+	return Personas.find({});
+});
+
+Meteor.publish('personas-by-name', function(name) {
+	return Personas.find({name: name});
+});
+
+Meteor.method('createPersona', function(json) {
+	Personas.insert({
 		name: json.name,
 		createdAt: new Date(),
 	});
 
+});
+
+/***********************************************************/
+
+Meteor.publish('accounts', function() {
+	return Accounts.find({});
+});
+
+Meteor.method('createAccount', function(json) {
+	Accounts.insert({
+		name: json.name,
+		description: json.description,
+		createdAt: new Date(),
+		balance: json.balance
+	});
 });
